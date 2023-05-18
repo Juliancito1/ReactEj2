f57d075d8da7b4262935b88c341f386d654bf80c
Julian <julianser@hotmail.com.ar>
2023-05-18 03:37:18 -0300

    Se realizo el ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Helloworldprops from './components/Helloworldprops'
 
 function App() {
 
   return (
     <>
-      
+      <Helloworldprops myfriend='my friend!'/>
     </>
   )
 }
---
+++ src/components/Helloworldprops.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Helloworldprops = ({myfriend}) => {
+    return (
+        <section>
+            <h1>Hello {myfriend}</h1>
+        </section>
+    );
+};
+
+export default Helloworldprops;
